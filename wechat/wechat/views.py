ec03cf954a5d06d2fd31079da73ee00a17b89681
tonyqiu1019 <tq7bw@virginia.edu>
2017-10-20 15:53:15 -0400

    test #9

--- wechat/wechat/views.py
+++ wechat/wechat/views.py
@@ -40,12 +40,12 @@ def _check_token(request):
         return HttpResponseBadRequest('something went wrong')
 
 
-def _reply(from_name, to_name, create_time, msg_type, content):
+def _reply(from_name, to_name, create_time, content):
     ret = etree.Element('xml')
     etree.SubElement(ret, 'FromUserName').text = from_name
     etree.SubElement(ret, 'ToUserName').text = to_name
     etree.SubElement(ret, 'CreateTime').text = create_time
-    etree.SubElement(ret, 'MsgType').text = msg_type
+    etree.SubElement(ret, 'MsgType').text = 'text'
     etree.SubElement(ret, 'Content').text = content
 
     ret_str = etree.tostring(ret, pretty_print=True, encoding='unicode')
@@ -65,7 +65,7 @@ def _handle_reply(request):
 
     if msg_type != 'text':
         txt = '弹幕仅支持文本信息，请按\n\"弹幕 想发送的内容\"\n格式发弹幕'
-        return _reply(to_name, from_name, create_time, msg_type, txt)
+        return _reply(to_name, from_name, create_time, txt)
 
     try:
         content = tree.xpath('/xml/Content')[0].text
@@ -74,15 +74,15 @@ def _handle_reply(request):
 
     if len(content) <= 2 or content[:2] != '弹幕':
         txt = '你的弹幕格式似乎不对哦，请按\n\"弹幕 想发送的内容\"\n格式发弹幕'
-        return _reply(to_name, from_name, create_time, msg_type, txt)
+        return _reply(to_name, from_name, create_time, txt)
 
     bullet_txt = content[2:].strip()
     if len(bullet_txt) == 0:
         txt = '不能发送空弹幕，请按\n\"弹幕 想发送的内容\"\n格式发弹幕'
-        return _reply(to_name, from_name, create_time, msg_type, txt)
+        return _reply(to_name, from_name, create_time, txt)
     else:
         txt = '你发送的弹幕信息是：' + bullet_txt
-        return _reply(to_name, from_name, create_time, msg_type, txt)
+        return _reply(to_name, from_name, create_time, txt)
 
 
 def index_page(request):
