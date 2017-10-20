bb4f3622ddad8186e6efb0285a21091d8701cfd2
tonyqiu1019 <tq7bw@virginia.edu>
2017-10-21 03:09:53 +0800

    fix encoding bug

--- wechat/wechat/views.py
+++ wechat/wechat/views.py
@@ -34,4 +34,5 @@ def index_page(request):
         etree.SubElement(ret_tree, 'CreateTime').text = tree.xpath('/xml/CreateTime')[0].text
         etree.SubElement(ret_tree, 'MsgType').text = tree.xpath('/xml/MsgType')[0].text
         etree.SubElement(ret_tree, 'Content').text = '你好'
-        return HttpResponse(etree.tostring(ret_tree, pretty_print=True), content_type='application/xml')
+        ret = etree.tostring(ret_tree, pretty_print=True, encoding='unicode')
+        return HttpResponse(ret, content_type='application/xml')
