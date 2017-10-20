7778c89b493591077f6f32e24dd7342e651501d2
tonyqiu1019 <tq7bw@virginia.edu>
2017-10-20 15:31:44 -0400

    test #3

--- wechat/wechat/views.py
+++ wechat/wechat/views.py
@@ -3,6 +3,24 @@ from django.http import HttpResponse, HttpResponseBadRequest
 from hashlib import sha1
 from lxml import etree
 
+import urllib.request
+import json
+
+def _make_get_request(url):
+    req = urllib.request.Request(url)
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+    return resp
+
+
+def _make_post_request(url, post_data):
+    post_encoded = urllib.parse.urlencode(post_data).encode('utf-8')
+    req = urllib.request.Request(url, data=post_encoded, method='POST')
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+    return resp
+
+
 def _check_token(request):
     get_dict = request.GET.dict()
     needed_token = ['signature', 'timestamp', 'nonce', 'echostr']
@@ -22,17 +40,37 @@ def _check_token(request):
         return HttpResponseBadRequest('something went wrong')
 
 
-def index_page(request):
-    if request.method == 'GET':
-        return _check_token(request)
+def _reply(from_name, to_name, create_time, msg_type, content):
+    ret = etree.Element('xml')
+    etree.SubElement(ret, 'FromUserName').text = from_name
+    etree.SubElement(ret, 'ToUserName').text = to_name
+    etree.SubElement(ret, 'CreateTime').text = create_time
+    etree.SubElement(ret, 'MsgType').text = msg_type
+    etree.SubElement(ret, 'Content').text = content
 
-    if request.method == 'POST':
-        tree = etree.fromstring(request.body.decode('utf-8'))
-        ret_tree = etree.Element('xml')
-        etree.SubElement(ret_tree, 'ToUserName').text = tree.xpath('/xml/FromUserName')[0].text
-        etree.SubElement(ret_tree, 'FromUserName').text = tree.xpath('/xml/ToUserName')[0].text
-        etree.SubElement(ret_tree, 'CreateTime').text = tree.xpath('/xml/CreateTime')[0].text
-        etree.SubElement(ret_tree, 'MsgType').text = tree.xpath('/xml/MsgType')[0].text
-        etree.SubElement(ret_tree, 'Content').text = '你好'
-        ret = etree.tostring(ret_tree, pretty_print=True, encoding='unicode')
-        return HttpResponse(ret, content_type='application/xml')
+    ret_str = etree.tostring(ret, pretty_print=True, encoding='unicode')
+    return HttpResponse(ret_str, content_type='application/xml')
+
+
+def _handle_reply(request):
+    tree = etree.fromstring(request.body.decode('utf-8'))
+    try:
+        from_name = tree.xpath('/xml/FromUserName')[0].text
+        to_name = tree.xpath('/xml/ToUserName')[0].text
+        create_time = tree.xpath('/xml/CreateTime')[0].text
+        msg_type = tree.xpath('/xml/MsgType')[0].text
+    except:
+        return HttpResponseBadRequest('cannot parse correct xml')
+
+    if msg_type != 'text':
+        txt = '弹幕仅支持文本信息，请按\"弹幕 <想发送的内容>\"格式发弹幕'
+        return _reply(to_name, from_name, create_time, msg_type, txt)
+    else:
+        txt = '弹幕仅支持文本信息，请按\"弹幕 <想发送的内容>\"格式发弹幕'
+        return _reply(to_name, from_name, create_time, msg_type, txt)
+
+
+def index_page(request):
+    if request.method == 'GET': return _check_token(request)
+    if request.method == 'POST': return _handle_reply(request)
+    return HttpResponseBadRequest('bad request type')
