a11fc6ae9c344a49585e8d0f78e9700f9b257937
tonyqiu1019 <tq7bw@virginia.edu>
2017-11-10 17:55:42 -0500

    uodate reply rule

--- wechat/wechat/views.py
+++ wechat/wechat/views.py
@@ -48,56 +48,58 @@ def _reply(from_name, to_name, create_time, content):
 
 def _handle_reply(request):
     tree = etree.fromstring(request.body.decode('utf-8'))
-
     try:
         from_name = tree.xpath('/xml/FromUserName')[0].text
         to_name = tree.xpath('/xml/ToUserName')[0].text
         create_time = tree.xpath('/xml/CreateTime')[0].text
         msg_type = tree.xpath('/xml/MsgType')[0].text
     except:
-        return HttpResponseBadRequest('cannot parse correct xml')
+        return HttpResponse('')
 
     if msg_type == 'event':
         try:
             event = tree.xpath('/xml/Event')[0].text
             if event == 'subscribe':
-                txt = '欢迎关注UVA CSSS微信公众号！\n今晚的好声音活动中，我们将使用此公众号为观众提供弹幕互动和投票功能，请按照\n\"弹幕 想发送的内容\"\n格式发弹幕，或回复\"投票\"为喜欢的歌手投票！感谢你的参与！'
+                txt = '欢迎关注UVA CSSS微信公众号！\n今晚的好声音决赛中，我们将使用此公众号为观众提供弹幕互动和投票功能，请按照\n\"弹幕 想发送的内容\"\n格式发送弹幕。今晚的比赛中，我们将进行两轮演唱，请在第一轮结束后发送\"最佳组合\"支持你最喜欢的组合，或在第二轮结束后发送\"最佳歌手\"支持你心目中的歌手。你的投票将决定最后出线参与VA好声音的选手名额。感谢你的参与！'
                 return _reply(to_name, from_name, create_time, txt)
             else:
                 return HttpResponse('')
         except:
             return HttpResponse('')
     elif msg_type != 'text':
-        txt = '目前仅支持文本信息，请按\n\"弹幕 想发送的内容\"\n格式发弹幕，或回复\"投票\"为喜欢的歌手投票'
+        txt = '目前仅支持文本信息，请按\n\"弹幕 想发送的内容\"\n格式发弹幕，回复\"高级弹幕\"获取发弹幕教程，或回复\"最佳组合\"、\"最佳歌手\"参与投票'
         return _reply(to_name, from_name, create_time, txt)
 
     try:
         content = tree.xpath('/xml/Content')[0].text
     except:
-        return HttpResponseBadRequest('cannot parse correct xml')
+        return HttpResponse('')
 
-    if content == '教程':
+    if content == '高级弹幕' or content == '教程':
         txt = '发送弹幕请参考：http://mp.weixin.qq.com/s/pPOxYWzgmnXjWN6Dienf6Q'
         return _reply(to_name, from_name, create_time, txt)
-    if content == '投票':
-        txt = '请点击以下链接：http://mp.weixin.qq.com/s/vaq3mBpZXWwqAxenun5Y9g 参与投票！'
+    if content == '最佳组合':
+        txt = '最佳组合的投票链接暂未开放哦！'
+        return _reply(to_name, from_name, create_time, txt)
+    if content == '最佳歌手':
+        txt = '最佳歌手的投票链接暂未开放哦！'
         return _reply(to_name, from_name, create_time, txt)
 
     if len(content) <= 2 or content[:2] != '弹幕':
-        txt = '你的消息格式似乎不对哦，请按\n\"弹幕 想发送的内容\"\n格式发弹幕，回复\"教程\"获取发弹幕教程，或回复\"投票\"为喜欢的歌手投票'
+        txt = '你的消息格式似乎不对哦，请按\n\"弹幕 想发送的内容\"\n格式发弹幕，回复\"高级弹幕\"获取发弹幕教程，或回复\"最佳组合\"、\"最佳歌手\"参与投票'
         return _reply(to_name, from_name, create_time, txt)
 
     bul = content[2:]
     if len(bul) == 0:
-        txt = '不能发送空弹幕，请按\n\"弹幕 想发送的内容\"\n格式发弹幕'
+        txt = '不能发送空弹幕，请按\n\"弹幕 想发送的内容\"\n格式发弹幕，回复\"高级弹幕\"获取发弹幕教程，或回复\"最佳组合\"、\"最佳歌手\"参与投票'
         return _reply(to_name, from_name, create_time, txt)
     if bul[0] != ' ':
-        txt = '你的消息格式似乎不对哦，请按\n\"弹幕 想发送的内容\"\n格式发弹幕，回复\"教程\"获取发弹幕教程，或回复\"投票\"为喜欢的歌手投票'
+        txt = '你的消息格式似乎不对哦，请按\n\"弹幕 想发送的内容\"\n格式发弹幕，回复\"高级弹幕\"获取发弹幕教程，或回复\"最佳组合\"、\"最佳歌手\"参与投票'
         return _reply(to_name, from_name, create_time, txt)
 
     bul = bul.strip()
     if len(bul) == 0:
-        txt = '不能发送空弹幕，请按\n\"弹幕 想发送的内容\"\n格式发弹幕'
+        txt = '不能发送空弹幕，请按\n\"弹幕 想发送的内容\"\n格式发弹幕，回复\"高级弹幕\"获取发弹幕教程，或回复\"最佳组合\"、\"最佳歌手\"参与投票'
         return _reply(to_name, from_name, create_time, txt)
 
     post_url = 'https://danmu-183606.appspot.com/api/create/'
@@ -109,10 +111,10 @@ def _handle_reply(request):
             txt = '弹幕发送成功！'
             return _reply(to_name, from_name, create_time, txt)
         else:
-            txt = '你已被禁言，请联系管理员，询问情况后再试'
+            txt = '你已被禁言，请联系管理员，询问情况后再试；也可以回复\"最佳组合\"、\"最佳歌手\"参与投票！'
             return _reply(to_name, from_name, create_time, txt)
     except:
-        txt = 'oops，你的弹幕发送失败了...请稍等片刻再试哦！回复\"教程\"获取发弹幕教程，或回复\"投票\"为喜欢的歌手投票'
+        txt = 'oops，你的弹幕发送失败了...请稍等片刻再试哦！回复\"高级弹幕\"获取发弹幕教程，或回复\"最佳组合\"、\"最佳歌手\"参与投票'
         return _reply(to_name, from_name, create_time, txt)
 
 
